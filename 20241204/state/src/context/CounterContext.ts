0f4727bf6ccb26c226262c9751b2a220bf85b4bf
kisudevel <kisudevel@gmail.com>
2024-12-04 10:45:15 +0900

    20241204-1

---
+++ 20241204/state/src/context/CounterContext.ts
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+export const CounterContext = createContext<CounterContextType | null>(null);
+export const CounterActionContext =
+  createContext<CounterActionContextType | null>(null);
