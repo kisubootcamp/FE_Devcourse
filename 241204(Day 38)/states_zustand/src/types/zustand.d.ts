02d1dbbefc7c9c0ae94ee2adf0ac899b3a9c5d76
songwon-sung <swsung91@gmail.com>
2024-12-04 16:32:40 +0900

    20241204_수업내용

--- 241204(Day 38)/states_zustand/src/types/zustand.d.ts
+++ 241204(Day 38)/states_zustand/src/types/zustand.d.ts
@@ -4,3 +4,15 @@ interface CounterStoreType {
   decrement: () => void;
   reset: () => void;
 }
+
+interface User {
+  name: string;
+  age: number;
+}
+
+interface AuthStoreType {
+  user: User | null;
+  isLoggedIn: boolean;
+  login: (user: User) => void;
+  logout: () => void;
+}
