747042002b99a2cce18d29a6278a6ee78de62b98
denshou <zecaumr@naver.com>
2024-12-04 09:03:49 +0900

    20241203_TIL_복습

--- 20241203/memo-re/src/components/CompoA.tsx
+++ 20241203/memo-re/src/components/CompoA.tsx
@@ -1,20 +1,17 @@
-import { useCallback, useState } from "react";
+import { useContext } from "react";
 import CompoB from "./CompoB";
+import { CounterContext } from "../context/CounterContext";
 
 export default function CompoA() {
-  const [count, setCount] = useState(0);
-  const increment = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, []);
+  const { count } = useContext(CounterContext)!;
   console.log("A rendering");
   return (
     <>
       <h1>A</h1>
       <div>
         <div>{count}</div>
-        <button onClick={() => setCount((prev) => prev + 1)}>A증가</button>
       </div>
-      <CompoB increment={increment} />
+      <CompoB />
     </>
   );
 }
--- 20241203/memo-re/src/components/CompoB.tsx
+++ 20241203/memo-re/src/components/CompoB.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import CompoC from "./CompoC";
+import { CounterActionContext } from "../context/CounterContext";
 
-export default React.memo(function CompoB({increment}:{ increment: () => void; }) {
+export default React.memo(function CompoB() {
+  const { increment, decrement, reset } = useContext(CounterActionContext)!;
   console.log("B rendering");
   return (
     <>
       <h1>B</h1>
+      <button onClick={increment}>증가</button>
+      <button onClick={decrement}>감소</button>
+      <button onClick={reset}>리셋</button>
       <CompoC />
     </>
   );
