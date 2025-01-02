c45820ef3c46d3d65c70dd618c284f9acc673403
naehyun <scott253@naver.com>
2025-01-02 17:45:49 +0900

    250102_과제제출

---
+++ 250102/src/components/Ex1_1.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "Ex1",
+  data() {
+    return {
+      active: false,
+      onclick: () => {
+        this.active = true;
+      },
+    };
+  },
+};
+</script>
+<template>
+  <button :onclick>버튼</button>
+  <div v-if="active">조건이 참입니다!</div>
+</template>
+<style scoped></style>
---
+++ 250102/src/components/Ex2_4.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      status: "loggedIn", //"loggedIn", "loggedOut", "banned"
+      account: "basic", // basic, premium
+    };
+  },
+};
+</script>
+<template>
+  <h1 v-if="status === 'loggedIn' && account === 'basic'">
+    기본 계정으로 로그인 중
+  </h1>
+  <h1 v-else-if="status === 'loggedIn' && account === 'premium'">
+    프리미엄 계정으로 로그인 중
+  </h1>
+  <h1 v-else-if="status === 'loggedOut'">로그인 필요</h1>
+  <h1 v-else-if="status === 'banned'">계정정지</h1>
+</template>
+<style scoped></style>
