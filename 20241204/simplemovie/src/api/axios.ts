855b3a04501ad34cb5fa9bddb96b693f0eea5c26
ChoiYoungKyu <mpd253@naver.com>
2025-01-02 15:22:29 +0900

    20250102_과제제출

--- 20241204/simplemovie/src/api/axios.ts
+++ 20241204/simplemovie/src/api/axios.ts
@@ -4,6 +4,5 @@ export const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3/movie",
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDkzYTllZTk2YzUwODlkMmYxNTNlNDdiOWU4YzBhYSIsIm5iZiI6MTczMzI5MTMwNS4xNjgsInN1YiI6IjY3NGZlZDI5NjU2NzAyYTc5MmZmYWRlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gD5df8vcuE6tgc2uX7CbOeZ5qYOo8OCxnksHYI_SvA4",
   },
 });
