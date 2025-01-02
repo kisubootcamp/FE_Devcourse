2ca254ef6bb85c7a6fa36f1e19bf2383b678375a
최민혁 <130155327+lactofreee@users.noreply.github.com>
2025-01-02 15:07:04 +0900

    ✨ feat: 01-02 LV1

---
+++ 20250102/01-02/src/components/LV2.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "LV2",
+  data() {
+    return {
+      state: "loading",
+      loading: "데이터를 로딩 중입니다...",
+      error: "에러가 발생했습니다.",
+      success: "데이터 로딩 완료!",
+      idle: "대기 중",
+    };
+  },
+};
+</script>
+<template>
+  <div>
+    <p v-if="state === 'loading'">{{ loading }}</p>
+    <p v-else-if="state === 'error'">{{ error }}</p>
+    <p v-else-if="state === 'success'">{{ success }}</p>
+    <p v-else>{{ idle }}</p>
+  </div>
+  <div>
+    
+  </div>
+</template>
