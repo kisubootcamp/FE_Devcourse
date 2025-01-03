13630c760dfe2db3e4ae363e7c5dca2edc357714
Kim, DongChan <kdc9050@naver.com>
2025-01-03 11:06:01 +0900

    20250103_과제제출

---
+++ 20250103/src/components/EX3_3.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "EX3_3",
+  data() {
+    return {
+      items: ["Apple", "Banana", "Cherry"],
+    };
+  },
+};
+</script>
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ index }} - {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+<style></style>
---
+++ 20250103/src/components/EX4_4.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "EX4_4",
+  data() {
+    return {
+      tasks: [
+        { title: "Vue 공부하기", completed: false },
+        { title: "프로젝트 작업", completed: true },
+        { title: "운동하기", completed: false },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task.title">
+        <input type="checkbox" v-model="task.completed" />
+        <span
+          :style="{ textDecoration: task.completed ? 'line-through' : 'none' }"
+          >{{ task.title }}</span
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+<style></style>
