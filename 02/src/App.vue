58b819ff5f3de303f0aa6c7801b86a70569e395b
quokka <ansagi3@naver.com>
2025-01-03 11:56:50 +0900

    ✔ lv4 까지 풀이

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -9,6 +9,11 @@ import EX7 from "./components/EX7.vue";
 import EX8 from "./components/EX8.vue";
 import EX9 from "./components/EX9.vue";
 import EX10 from "./components/EX10.vue";
+import EX4_1 from "./components/EX4_1.vue";
+import EX4_2 from "./components/EX4_2.vue";
+import EX4_3 from "./components/EX4_3.vue";
+import EX4_4 from "./components/EX4_4.vue";
+import EX4_5 from "./components/EX4_5.vue";
 
 export default {
   name: "App",
@@ -23,6 +28,11 @@ export default {
     EX8,
     EX9,
     EX10,
+    EX4_1,
+    EX4_2,
+    EX4_3,
+    EX4_4,
+    EX4_5,
   },
   data() {
     return {};
@@ -40,5 +50,10 @@ export default {
   <<EX8 />
   <EX9 />
   <EX10 />
+  <EX4_1 />
+  <EX4_2 />
+  <EX4_3 />
+  <EX4_4 />
+  <EX4_5 />
 </template>
 <style scoped></style>
---
+++ 02/src/components/EX4_5.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      teamMembers: [
+        { name: "홍길동", role: "개발자" },
+        { name: "김철수", role: "디자이너" },
+        { name: "이영희", role: "개발자" },
+        { name: "박지민", role: "기획자" },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <ul>
+    <li
+      v-for="(members, key) in teamMembers.reduce((acc, member) => {
+        if (!acc[member.role]) {
+          acc[member.role] = [];
+        }
+        acc[member.role].push(member);
+        return acc;
+      }, {})"
+      :key="key"
+    >
+      <h3>{{ key }}</h3>
+      <ul>
+        <li v-for="(member, index) in members" :key="index">
+          {{ member.name }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
