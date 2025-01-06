c27eb8392309d37d147b9b55b95ece2cf463bd48
naehyun <scott253@naver.com>
2025-01-06 15:01:58 +0900

    [FE3_김내현] 20250106_과제제출2

--- 250106/login/src/App.vue
+++ 250106/login/src/App.vue
@@ -51,17 +51,18 @@
                 v-model="remember"
               />
             </div>
-            <label for="remember" class="ms-2 text-sm font-medium text-gray-900"
+            <label for="remember" class="text-sm font-medium text-gray-900 ms-2"
               >Remember me</label
             >
           </div>
-          <a href="#" class="ms-auto text-sm text-blue-700 hover:underline"
+          <a href="#" class="text-sm text-blue-700 ms-auto hover:underline"
             >Lost Password?</a
           >
         </div>
         <button
           type="submit"
           class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:cursor-not-allowed"
+          :disabled="isdisabled"
           @click.prevent="view"
         >
           Login to your account
@@ -83,13 +84,34 @@ export default {
       email: "",
       password: "",
       remember: false,
+      emailRegex: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+      passwordRegex: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[\W_]).{4,}$/,
     };
+  },
+  computed: {
+    isdisabled() {
+      return !this.email || !this.password;
+    },
   },
   methods: {
     view() {
-      console.log(this.email);
-      console.log(this.password);
-      console.log(this.remember);
+      if (!this.emailRegex.test(this.email)) {
+        alert("Please enter a valid email address.");
+        return;
+      }
+
+      if (!this.passwordRegex.test(this.password)) {
+        alert(
+          "Password must be at least 4 characters long and contain at least one letter and one number."
+        );
+        return;
+      }
+
+      if (this.remember) {
+        alert("Remember me is checked!");
+      }
+
+      alert("Form submitted successfully!");
     },
   },
 };
