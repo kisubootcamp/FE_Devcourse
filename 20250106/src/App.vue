cf07ee90272fc6f6eadd16646abc0de06536a576
junyoung <jadw9715@gmail.com>
2025-01-06 14:22:54 +0900

    [FEAT] Level 2

--- 20250106/src/App.vue
+++ 20250106/src/App.vue
@@ -8,6 +8,11 @@ export default {
       remember: false,
     };
   },
+  computed: {
+    isLoginable() {
+      return !(this.email.length > 0 && this.pw.length > 0);
+    },
+  },
   methods: {
     handleSubmit() {
       console.log("submit", this.email, this.pw, this.remember);
@@ -80,6 +85,7 @@ export default {
         <button
           type="submit"
           class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:cursor-not-allowed"
+          :disabled="isLoginable"
         >
           Login to your account
         </button>
