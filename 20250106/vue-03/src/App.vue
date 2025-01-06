9241eb17f3b42ca68343172791e1b2b8bb1c9951
AAminha <dks4857@gmail.com>
2025-01-06 14:24:26 +0900

    20250106_vue_로그인폼_level2_제출

--- 20250106/vue-03/src/App.vue
+++ 20250106/vue-03/src/App.vue
@@ -15,6 +15,11 @@
         console.log('Remember me: ', this.remember);
       },
     },
+    computed: {
+      isDisabled() {
+        return !this.email || !this.password;
+      },
+    },
   };
 </script>
 <template>
@@ -81,7 +86,8 @@
         </div>
         <button
           type="submit"
-          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:cursor-not-allowed">
+          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:cursor-not-allowed"
+          :disabled="isDisabled">
           Login to your account
         </button>
         <div class="text-sm font-medium text-gray-500">
