3128dc55a692d4b0815b175edad9c50da2e6ff44
engsal <imaria0218@gmail.com>
2025-01-06 16:00:38 +0900

    20250106 오후 과제제출 리스트렌더링 김예빈

---
+++ 20250106/listrender/src/App.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+</template>
+<style></style>
--- 20250106/login/src/App.vue
+++ 20250106/login/src/App.vue
@@ -2,136 +2,80 @@
 export default {
   data() {
     return {
-      loginEmail: "",
-      loginPwd: "",
-      isRememberChecked: false,
-      rememberCheckedOnce: false,
+      customerList: [
+        { name: "Neil Sims", email: "email@windster.com", amount: 320 },
+        { name: "James", email: "james@gmail.com", amount: 250 },
+        { name: "Smith", email: "smith@gmail.com", amount: 600 },
+        { name: "Mike", email: "mike@gmail.com", amount: 200 },
+        { name: "Yarn", email: "yarn@gmail.com", amount: 140 },
+        { name: "Npm", email: "npm@gmail.com", amount: 220 },
+        { name: "Han", email: "han@gmail.com", amount: 450 },
+      ],
+      displayedCustomers: [],
+      isDisplayed: false,
     };
   },
   created() {
-    const savedEmail = localStorage.getItem("email");
-    if (savedEmail) {
-      this.loginEmail = savedEmail;
-      this.isRememberChecked = true;
-    }
+    this.displayedCustomers = this.customerList.slice(0, 5);
   },
   methods: {
-    handleSubmit() {
-      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-      const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{4,}$/;
-      // 이메일
-      if (!emailRegex.test(this.loginEmail)) {
-        alert("Please enter a valid email address");
-        return;
-      }
-      // 패스워드
-      if (!passwordRegex.test(this.loginPwd)) {
-        alert(
-          "Password must be at least 4 characters long and contain at least one letter and one number."
-        );
-        return;
-      }
-      // 리멤버
-      if (this.isRememberChecked && !this.rememberCheckedOnce) {
-        alert("Remember me is already checked!");
-        this.rememberCheckedOnce = true;
-        return;
-      }
-
-      if (this.isRememberChecked) {
-        localStorage.setItem("email", this.loginEmail);
-      } else {
-        localStorage.removeItem("email");
-      }
-
-      alert("Form submitted successfully!");
-      console.log(this.loginEmail);
-      console.log(this.loginPwd);
-      console.log(this.isRememberChecked);
-    },
-  },
-  computed: {
-    isButtonDisabled() {
-      return !(this.loginEmail && this.loginPwd);
+    handleViewAll() {
+      this.displayedCustomers = this.customerList;
+      this.isDisplayed = true;
     },
   },
 };
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-900">
+  <div class="flex flex-col items-center justify-center min-h-screen bg-black">
     <div
-      class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8"
+      class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700"
     >
-      <form class="space-y-6" @submit.prevent="handleSubmit">
-        <h5 class="text-xl font-medium text-gray-900">
-          Sign in to our platform
-        </h5>
-        <div>
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900"
-            >Your email</label
-          >
-          <input
-            v-model="loginEmail"
-            type="email"
-            name="email"
-            id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="name@company.com"
-            autocomplete="off"
-            required
-          />
-        </div>
-        <div>
-          <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900"
-            >Your password</label
-          >
-          <input
-            v-model="loginPwd"
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
-          />
-        </div>
-        <div class="flex items-start">
-          <div class="flex items-start">
-            <div class="flex items-center h-5">
-              <input
-                v-model="isRememberChecked"
-                id="remember"
-                type="checkbox"
-                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
-              />
-            </div>
-            <label for="remember" class="ms-2 text-sm font-medium text-gray-900"
-              >Remember me</label
-            >
-          </div>
-          <a href="#" class="ms-auto text-sm text-blue-700 hover:underline"
-            >Lost Password?</a
-          >
-        </div>
-        <button
-          type="submit"
-          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:cursor-not-allowed"
-          :disabled="isButtonDisabled"
+      <div class="flex items-center justify-between mb-4">
+        <h5
+          class="text-xl font-bold leading-none text-gray-900 dark:text-white"
         >
-          Login to your account
-        </button>
-        <div class="text-sm font-medium text-gray-500">
-          Not registered?
-          <a href="#" class="text-blue-700 hover:underline">Create account</a>
-        </div>
-      </form>
+          Latest Customers
+        </h5>
+        <a
+          v-if="!isDisplayed"
+          @click.prevent="handleViewAll"
+          href="#"
+          class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+        >
+          View all
+        </a>
+      </div>
+      <div class="flow-root">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+          <li
+            v-for="(customer, index) in displayedCustomers"
+            :key="index"
+            class="py-3 sm:py-4"
+          >
+            <div class="flex items-center">
+              <div class="flex-1 min-w-0 ms-4">
+                <p
+                  class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                >
+                  {{ customer.name }}
+                </p>
+                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                  {{ customer.email }}
+                </p>
+              </div>
+              <div
+                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
+              >
+                ${{ customer.amount }}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
