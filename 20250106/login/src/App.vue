fec849d3ac6512794b123d27772333428784f26c
최민혁 <130155327+lactofreee@users.noreply.github.com>
2025-01-06 14:27:08 +0900

    level2 구현

--- 20250106/login/src/App.vue
+++ 20250106/login/src/App.vue
@@ -8,6 +8,11 @@ export default {
       chk: false,
     };
   },
+  computed: {
+    isFormValid() {
+      return this.email.trim() !== "" && this.pw.trim() !== "";
+    },
+  },
 };
 </script>
 
@@ -75,6 +80,7 @@ export default {
         <button
           type="submit"
           class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:cursor-not-allowed"
+          :disabled="!isFormValid"
         >
           Login to your account
         </button>
