496d1fb9b749eb76d905ea4956dc52f1d228e605
yejiLee <dpwlals1017@gmail.com>
2025-01-06 22:05:35 +0900

    2025.01.03_폴더

---
+++ 2025.01.03/src/App.vue
@@ -0,0 +1,91 @@
+<script>
+import Ex3 from "./components/Ex3.vue";
+import Ex4 from "./components/Ex4.vue";
+import Vmodel from "./components/Vmodel.vue";
+export default {
+  name: "App",
+  components: {
+    Ex3,
+    Ex4,
+    Vmodel,
+  },
+  data() {
+    return {
+      categories: [
+        {
+          id: 1,
+          name: "전자제품",
+          product: [
+            {
+              id: 1,
+              name: "스마트폰",
+              isAvailable: true,
+              option: [
+                { id: 1, name: "블랙", inStock: true },
+                { id: 2, name: "화이트", inStock: true },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <Ex3 />
+  <hr />
+  <Ex4 />
+  <hr />
+  <Vmodel />
+  <hr />
+  <template v-for="category in categories" :key="category.id">
+    <h2>{{ category.name }}</h2>
+    <template v-for="product in category.product" :key="product.id">
+      <h3>{{ product.name }}</h3>
+      <template v-for="option in product.option" :key="option.id">
+        <span v-if="option.inStock">{{ option.name }}</span>
+      </template>
+    </template>
+  </template>
+</template>
+<style scoped></style>
+
+<!-- 스타일적용하기 -->
+<!-- <script setup>
+import { styled } from "@vue-styled-components/core";
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  border-radius: 9999px;
+  background-color: #4c5a6d;
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+`;
+</script>
+
+<template>
+  <StyledDiv>Hello World!</StyledDiv>
+</template> -->
+
+<!-- <script>
+import First from "./components/First.vue";
+
+export default {
+  name: "App",
+  components: {
+    First,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <button class="btn-primary">로그인</button>
+</template>
+<style scoped></style> -->
---
+++ 2025.01.03/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/css/index.css";
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.css";
+createApp(App).mount("#app");
