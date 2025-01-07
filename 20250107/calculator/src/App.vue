303ee16b6f2cb2c4f7bbd2e94b7723b0a77a9f6b
hyunw00 <khwland090@gmail.com>
2025-01-07 10:28:13 +0900

    [FE3_김현우]_20250107 과제제출

---
+++ 20250107/calculator/src/App.vue
@@ -0,0 +1,108 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      input: "",
+      value: 0,
+      isShown: true,
+      operator: "",
+    };
+  },
+  methods: {
+    handleChange(event) {
+      this.isShown = true;
+      this.input += event.target.value;
+    },
+    handleInit() {
+      this.input = "";
+      this.value = "";
+    },
+    //  120 + 3 - 10 =
+    handleClickOperator(event) {
+      switch (this.operator) {
+        case "+":
+          this.value += Number(this.input);
+          this.input = "";
+          break;
+        case "-":
+          this.value -= Number(this.input);
+          this.input = "";
+          break;
+        case "*":
+          this.value *= Number(this.input);
+          this.input = "";
+          break;
+        case "/":
+          this.value /= Number(this.input);
+          this.input = "";
+          break;
+        default:
+          this.value = Number(this.input);
+          this.input = "";
+          break;
+      }
+
+      this.operator = event.target.value;
+      if (this.operator === "=") {
+        this.input = this.value;
+      }
+    },
+  },
+};
+</script>
+<template>
+  <div class="calculator">
+    <form name="forms" @click="handleChange">
+      <input type="text" name="output" readonly :value="isShown ? input : ''" />
+      <input type="button" class="clear" value="C" @click.stop="handleInit" />
+      <input
+        type="button"
+        class="operator"
+        value="/"
+        @click.stop="handleClickOperator"
+      />
+      <input type="button" value="1" />
+      <input type="button" value="2" />
+      <input type="button" value="3" />
+      <input
+        type="button"
+        class="operator"
+        value="*"
+        @click.stop="handleClickOperator"
+      />
+      <input type="button" value="4" />
+      <input type="button" value="5" />
+      <input type="button" value="6" />
+      <input
+        type="button"
+        class="operator"
+        value="+"
+        @click.stop="handleClickOperator"
+      />
+      <input type="button" value="7" />
+      <input type="button" value="8" />
+      <input type="button" value="9" />
+      <input
+        type="button"
+        class="operator"
+        value="-"
+        @click.stop="handleClickOperator"
+      />
+      <input
+        type="button"
+        class="dot"
+        value="."
+        @click.stop="handleClickOperator"
+      />
+      <input type="button" value="0" />
+      <input
+        type="button"
+        class="operator result"
+        value="="
+        @click.stop="handleClickOperator"
+      />
+    </form>
+  </div>
+</template>
+<style scoped></style>
