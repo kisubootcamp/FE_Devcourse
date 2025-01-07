55e5b573fa473e28dfd1c1fbeffc84b98317e604
junyoung <jadw9715@gmail.com>
2025-01-07 15:10:03 +0900

    [FE3_이준영] 20250107 로그인 폼 Level.4 과제 제출

---
+++ 20250107/login/src/App.vue
@@ -0,0 +1,124 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      email: "",
+      pw: "",
+      remember: false,
+    };
+  },
+  computed: {
+    isLoginable() {
+      return !(this.email.length > 0 && this.pw.length > 0);
+    },
+  },
+  methods: {
+    handleSubmit() {
+      // email 체크
+      const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+      if (!emailReg.test(this.email))
+        return alert("Please enter a valid email address.");
+
+      // pw 체크
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{4,}$/;
+      if (!passwordRegex.test(this.pw))
+        return alert(
+          "Password must be at least 4 characters long and contain at least one letter and one number."
+        );
+
+      // remember 체크하면 alert
+      if (this.remember) {
+        alert("Remember me is checked!");
+        localStorage.setItem("remember", this.email);
+      }
+
+      // 마무리
+      alert("Form submitted successfully!");
+    },
+  },
+  beforeMount() {
+    const remeberedEmail = localStorage.getItem("remember");
+    if (remeberedEmail) this.email = remeberedEmail;
+  },
+};
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-900">
+    <div
+      class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8"
+    >
+      <form class="space-y-6" action="#" @submit.prevent="handleSubmit">
+        <h5 class="text-xl font-medium text-gray-900">
+          Sign in to our platform
+        </h5>
+        <div>
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900"
+            >Your email</label
+          >
+          <input
+            v-model="email"
+            type="email"
+            name="email"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="name@company.com"
+            autocomplete="off"
+            required
+          />
+        </div>
+        <div>
+          <label
+            for="password"
+            class="block mb-2 text-sm font-medium text-gray-900"
+            >Your password</label
+          >
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            v-model="pw"
+          />
+        </div>
+        <div class="flex items-start">
+          <div class="flex items-start">
+            <div class="flex items-center h-5">
+              <input
+                id="remember"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+                v-model="remember"
+              />
+            </div>
+            <label for="remember" class="ms-2 text-sm font-medium text-gray-900"
+              >Remember me</label
+            >
+          </div>
+          <a href="#" class="ms-auto text-sm text-blue-700 hover:underline"
+            >Lost Password?</a
+          >
+        </div>
+        <button
+          type="submit"
+          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:cursor-not-allowed"
+          :disabled="isLoginable"
+        >
+          Login to your account
+        </button>
+        <div class="text-sm font-medium text-gray-500">
+          Not registered?
+          <a href="#" class="text-blue-700 hover:underline">Create account</a>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
