b38041fe95bca5ddea669efa7716d9a7efba049a
wl990@naver.com <wl990@naver.com>
2025-01-09 15:52:47 +0900

    [ FE3_안지원] 20250109_과제제출

---
+++ 20250109/06/src/components/Count.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { inject } from "vue";
+
+const props = defineProps({
+  count: {
+    type: Number,
+    rejected: true,
+    default() {
+      return 10;
+    },
+    validator(value) {
+      return value > 0;
+    },
+  },
+});
+
+const emite = defineEmits(["increment"]);
+const increment = () => {
+  emite("increment", 120);
+};
+
+const provideCount = inject("provideCount");
+</script>
+
+<template>
+  <div>{{ props.count }}</div>
+  <button @click="increment">클릭</button>
+  <h1>provideCount: {{ provideCount }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ 20250109/06/src/components/Setup.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed, ref } from "vue";
+
+const count = ref(0);
+const doubleCount = computed(() => count.value * 2);
+const decrement = () => count.value--;
+const increment = () => count.value++;
+const reset = () => (count.value = 0);
+</script>
+
+<template>
+  <div>{{ count }}</div>
+  <div>doubleCount {{ doubleCount }}</div>
+  <button @click="decrement">감소</button>
+  <button @click="reset">리셋</button>
+  <button @click="increment">증가</button>
+</template>
+
+<style lang="scss" scoped></style>
