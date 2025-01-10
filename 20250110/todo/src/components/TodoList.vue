7bf851f46f1050a2c25b8b3d715ebb8dd9f99cf9
kangsuyeong <ekdh16@naver.com>
2025-01-10 20:44:28 +0900

    20250110_todo(props)

---
+++ 20250110/todo/src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TodoListItem from "./TodoListItem.vue";
+
+const props = defineProps({
+  todos: Array,
+  toggleTodo: Function,
+  removeTodo: Function,
+});
+</script>
+<template>
+  <ul className="divide-y divide-gray-200">
+    <TodoListItem
+      v-for="(item, index) in todos"
+      :key="index"
+      :item
+      :toggleTodo
+      :removeTodo
+    />
+  </ul>
+</template>
+<style></style>
