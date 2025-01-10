d9b8e6ab8e54d653a817cdabaee31904a37bedd0
AAminha <dks4857@gmail.com>
2025-01-10 16:22:48 +0900

    20250108 할 일 관리 앱  - props drilling 과제제출

---
+++ 20250110/todo-props-drilling/src/components/Todo.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { reactive } from 'vue';
+  import TodoEditor from './TodoEditor.vue';
+  import TodoHeader from './TodoHeader.vue';
+  import TodoList from './TodoList.vue';
+
+  const todoList = reactive([]);
+
+  const addTodo = (todo) => {
+    todoList.push(todo);
+  };
+
+  const removeTodo = (index) => {
+    todoList.splice(index, 1);
+  };
+
+  const toggleDone = (index) => {
+    todoList[index].done = !todoList[index].done;
+  };
+</script>
+<template>
+  <div className="max-w-md mx-auto shadow-lg rounded-lg overflow-hidden">
+    <TodoHeader />
+    <TodoEditor @addTodo="addTodo" />
+    <TodoList
+      :todoList="todoList"
+      @removeTodo="removeTodo"
+      @toggleDone="toggleDone" />
+  </div>
+</template>
+<style></style>
