6015303f1a19885bc14b2c60e0272d6f2469a9c7
sunhyeong <parksunhyeong529@gmail.com>
2025-01-11 14:29:11 +0900

    todo주말과제

---
+++ 20250110/todoProps/src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TodoListItem from "./TodoListItem.vue";
+
+const props = defineProps({
+  todos: Array,
+  deleteTodo: Function,
+  toggleTodo: Function,
+});
+</script>
+<template>
+  <ul className="divide-y divide-gray-200">
+    <li v-for="todo in props.todos" :key="todo.id">
+      <TodoListItem
+        :todo="todo"
+        :delete-todo="deleteTodo"
+        :toggle-todo="toggleTodo"
+      />
+    </li>
+  </ul>
+</template>
+<style></style>
